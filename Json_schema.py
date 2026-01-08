98919bcbfc26be20768e436fe9d4fb7774e41afe
kooursreelekha <kooursreelekha@gmail.com>
2026-01-08 12:24:25 +0530

    initial commit

---
+++ Json_schema.py
@@ -0,0 +1,51 @@
+# JSON Schema support requires the inference API to support tool/function calling and structured response enforcement. Hugging Face’s inference APIs do not support that protocol.
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from typing import TypedDict
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+model=HuggingFaceEndpoint(
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.7,
+    huggingfacehub_api_token=os.getenv("HUGGINGFACE_API_KEY")  # Load from .env
+)
+
+chat = ChatHuggingFace(llm=model)
+
+#schema
+json_schema={
+    "title":"Review",
+    "type":"object",
+    "properties":{
+        "Summary":{
+            "type":"string",
+            "description":"Give basic summary of review"
+        },
+        "sentiment":{
+            "type":"string",
+            "enum":["pos","neg"],
+            "description":"give the tone of review either positive or negative"
+        }
+    }
+
+}
+
+structured_model=chat.with_structured_output(json_schema)
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh
+""")
+print(result)
